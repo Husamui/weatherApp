65c2bf9d3d54c01e1cff35e559439114a10d47c6
Husam Alrubaye <husamalrubaye@gmail.com>
2015-12-21 18:10:07 -0800

    styling perfection, adding the google mapis API, better search restuls and update the maps, and weather details base on the user input

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,22 @@
 var app = angular.module('app',[]);
 
-
 app.controller('weather', ['$scope','$http',function($scope,$http){
 
-    var mapsApiKey = 'AIzaSyC6PIpDgxcwJoPywvsDgDAUbWD1iQAqrWM';
+    // const variables
     var weatherApiKey = 'c04a9512cf2f19dfcf7709bb07fcc4c5';
     var defaultCity = 'San-francisco';
     var zipReg = /\d{5}/g;
+
+    // Get User Location from the Browser
     var options = {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0
     };
 
-
     function success(pos) {
         var crd = pos.coords;
         requestWeather('lat='+ crd.latitude + '5&lon=' + crd.longitude);
-
         initMap(crd.latitude, crd.longitude);
     }
 
@@ -27,25 +26,30 @@ app.controller('weather', ['$scope','$http',function($scope,$http){
     }
 
     function initLocation(){
-
         if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition(success, error, options);
         }else{
             requestWeather('q='+defaultCity);
             
         }
-
     }
     initLocation();
 
+    // Update restuls base on search input
     $scope.getweather = function() {
-
-        requestWeather(validateLocation($scope.address));
+        if ($scope.showSearch == true){
+            requestWeather(validateLocation($scope.address));
+            $scope.showSearch = false;
+            $scope.address = "";
+        }else{
+            $scope.showSearch = true;
+            document.getElementById("address-input").focus();
+        }
     };
 
     function validateLocation(location){
-
-        if (location != undefined){
+        if (location != undefined && location.length > 1){
+            console.log(location);
             if (location.match(zipReg) && location.length == 5){
                 console.log("it's zip code");
                 return 'zip='+location+',us';
@@ -54,15 +58,17 @@ app.controller('weather', ['$scope','$http',function($scope,$http){
                 return 'q='+location;
             }
         }else{
+            console.log('def' + typeof location);
             return 'q='+defaultCity;
+            //return false
         }
     }
 
     function requestWeather(location){
         $http.get('http://api.openweathermap.org/data/2.5/weather?' + location + '&units=metric&appid=' + weatherApiKey)
-
             .success(function(data, status, headers, config) {
                 $scope.weather = data;
+                initMap(data.coord.lat, data.coord.lon );
             })
             .error(function(data, status, headers, config) {
                 // log error
@@ -77,8 +83,11 @@ app.controller('weather', ['$scope','$http',function($scope,$http){
         });
     }
 
-
-
-
-
 }]);
+
+
+app.filter('rounded', function(){
+    return function(text){
+        return Math.round( parseInt(text) );
+    }
+});
